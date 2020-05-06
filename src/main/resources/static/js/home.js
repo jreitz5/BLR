c0443a805c446f9ad6bca72eb970d0a8a49d487f
Brandon <brandon_li@brown.edu>
2020-05-06 16:34:23 -0400

    added some javascript

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 			const str = "<div class=\"landlord\">"
 			+ "<img src=\"../images/landlordicon.png\" alt=\"landlord icon\" class=\"landlord-icon\">"
 			+ "<div class=\"landlord-content-wrapper\">"
-			+ "<h4>" + review[8] + " " + review[9] + "</h4>"
+			+ "<h3>" + review[8] + " " + review[9] + "</h3>"
 			+ "<img src=\"../images/quotation.png\" alt=\"opening quotation mark\" class=\"quotation\">"
 			+ "<div class=\"comment-wrapper\">"
 			+ "<a>" + review[5] + "</a>"
