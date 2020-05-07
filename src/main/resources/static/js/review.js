58e46500122f947130de2f00f2024964e5079cab
jreitz5 <56237801+jreitz5@users.noreply.github.com>
2020-05-07 02:48:48 -0400

    stars on submit_review are clickable and work

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     const $landlords = $("#landlords");
+    let rating = null;
     // Post suggestions
     $.post("/submit_review/data", jsonResponse => {
         const response = JSON.parse(jsonResponse);
@@ -22,7 +23,48 @@ $(document).ready(() => {
         landlordSelected.innerHTML = name;
         return;
     }
-
 });
 
+let five = document.getElementById("five");
+let four = document.getElementById("four");
+let three = document.getElementById("three");
+let two = document.getElementById("two");
+let one = document.getElementById("one");
+let stars = [one, two, three, four, five];
 
+let rating = null;
+
+//sets HTML on page load since star doesn't load properly normally
+stars.forEach( function(element) {
+    element.innerHTML = "☆"; 
+});
+
+function refreshStars(num){
+    stars.forEach( function(element) {
+        element.innerHTML = "☆"; 
+    });
+    for (let index = 0; index < num; index++) {
+        stars[index].innerHTML = "★";
+    }
+}
+
+function ffive(){
+    rating = 5;
+    refreshStars(rating);
+}
+function ffour(){
+    rating = 4;
+    refreshStars(rating);
+}
+function fthree(){
+    rating = 3;
+    refreshStars(rating);
+}
+function ftwo(){
+    rating = 2;
+    refreshStars(rating);
+}
+function fone(){
+    rating = 1;
+    refreshStars(rating);
+}
